1bf3e7bca9dffd92cd204239162993daa21e8d94
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-31 10:41:18 +0300

    get method added and db connected

---
+++ DapperSuperhero/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using DapperSuperhero.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperSuperhero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
+            return Ok(heroes);
+        }
+    }
+}
