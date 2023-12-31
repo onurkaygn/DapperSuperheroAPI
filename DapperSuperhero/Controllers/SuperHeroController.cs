8ad4543483a4134c0380f0e97e3de006dd1aa1ac
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2023-12-31 11:02:45 +0300

    get by id added

--- DapperSuperhero/Controllers/SuperHeroController.cs
+++ DapperSuperhero/Controllers/SuperHeroController.cs
@@ -2,7 +2,10 @@
 using DapperSuperhero.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace DapperSuperhero.Controllers
 {
@@ -17,12 +20,25 @@ namespace DapperSuperhero.Controllers
             _config = config;
         }
 
+        private SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
         {
-            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            using var connection = CreateConnection();
             var heroes = await connection.QueryAsync<SuperHero>("select * from superheroes");
             return Ok(heroes);
+        }
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetSuperHeroById(int heroId)
+        {
+            using var connection = CreateConnection();
+            var hero = await connection.QueryFirstAsync<SuperHero>("select * from superheroes where Id = @Id",
+                new {Id = heroId});
+            return Ok(hero);
         }
     }
 }
